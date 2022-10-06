0fa8f1321cdbfe3b25b0604f42c671b1911ce458
Ирина <mairisha@yandex.ru>
2022-10-06 12:20:04 +0300

    Finish Zadasga7

---
+++ Zadacha7/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("введите число:");
+int NumberY=Convert.ToInt32(Console.ReadLine());
+int Thirdnumber=NumberY/10;
+int SecondNumber=Thirdnumber%10;
+Console.WriteLine(SecondNumber);
